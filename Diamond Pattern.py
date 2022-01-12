4890de70038adb134d30b137b5eb1a597d20f475
Megha2004 <93330955+Megha2004@users.noreply.github.com>
2022-01-12 07:21:19 +0530

    Add files via upload

---
+++ Diamond Pattern.py
@@ -0,0 +1,13 @@
+n=int(input())
+for i in range(0,n):
+    for j in range(0,n-i-1):
+        print(end=' ')
+    for k in range(0,i+1):
+        print('*',end=' ')
+    print()
+for i in range(0,n):
+    for j in range(0,i+1):
+        print(end=' ')
+    for k in range(0,n-i-1):
+        print('*',end=' ')
+    print()
