e8983d65d03643ff2eb5537b7b2783748b1e58e0
Megha2004 <93330955+Megha2004@users.noreply.github.com>
2022-01-12 07:18:39 +0530

    Add files via upload

---
+++ Butterfly Pattern.py
@@ -0,0 +1,9 @@
+n=int(input())
+for i in range(1,n+1):
+    print('*'*i,end=' ')
+    print(' '*((n-i)*2),end=' ')
+    print("*"*i)
+for i in range(n,0,-1):
+    print('*'*i,end=' ')
+    print(' '*((n-i)*2),end=' ')
+    print("*"*i)
